d50f74e4826f88da886a79551f5d6c89f0dc534a
miarosianaa <miarosiana924@gmail.com>
2024-11-01 07:39:21 +0700

    SoalB

---
+++ Soal1.go
@@ -0,0 +1,26 @@
+//Mia Rosiana Santoso_2311102204
+
+package main
+import "fmt"
+
+func main() {
+	var nomor tiket int
+	var jumlah mahasiswa204 string
+	fmt.Scan(&jumlah mahaiswa204, &nomor tiket204)
+	cetakJumlahMahasiswa(nomor tiket204, jumlah mahasiswa204)
+}
+
+func cetakJumlahMahasiswa(nomor int, flag string) {
+	var jenis jumlah mahasiswa204 = " "
+
+	if flag == jumlah mahasiswa204 {
+		jenis = "225522"
+	} else if flag == 1 {
+		jenis = "tiket valid"
+	} else if flag == 0 {
+		jenis = "tiket tidak valid"
+		fmt.Println(nomor tiket, jumlah mahasiswa)
+
+	}
+
+}
---
+++ Soal3.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+
+func main(){
+	procedure cetak 
+	if x == 14
+	output (x)
+	else
+		output (x)
+		cetak (x+1)
+		end if
+
+	}
